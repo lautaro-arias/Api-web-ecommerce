d7a7052c3ee6e851da9cfac510ba0821ecc257dd
lautaro-arias <ariass00006@gmail.com>
2023-12-20 20:43:49 -0300

    v.07

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,55 +1,27 @@
-import  mercadopago,{ MercadoPagoConfig, Payment } from 'mercadopago';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { Request, Response, json } from 'express';
 
-
-
-//volver a hacer pruebas de todo
 export const payment = async (req:Request,res:Response) => {  
-    const client = new MercadoPagoConfig({ accessToken: 'TEST-4268166246905557-121907-d80f0d8df86261eeb064acc9165d864e-338813628', options: { timeout: 5000, idempotencyKey: 'abc' } });
+    const client =  new MercadoPagoConfig({ accessToken: 'TEST-7671113606844887-121907-4b453e26fe332f118153e22a75818419-1599227887',
+     options: { timeout: 5000 } 
+    });
 
-    const payment = new Payment(client);
+    const preference = new Preference(client);
+    
+    preference.create({ body: {
+        items: [
+            {
+            id: '1',
+            category_id: '',
+            currency_id: 'ARS',
+            description: 'darking',
+            title: 'remera',
+            quantity: 1,
+            unit_price: 100
+            }
+        ],
+    } }).then(console.log).catch(console.log);
 
-    const body = {
-        transaction_amount: 12.34,
-        description: 'remera',
-        payment_method_id: 'ARG',
-        payer: {
-            email: 'arias@gmail.com'
-        },
-    };
-    // Step 5: Make the request
-    payment.create({ body }).then(console.log).catch(console.log);
-    return res.status(200).json({body})
+
 
 }
-
-/* const client = new MercadoPagoConfig({
-        accessToken: 'TEST-4268166246905557-121907-d80f0d8df86261eeb064acc9165d864e-338813628',
-        options: { timeout: 5000, idempotencyKey: 'abc' }
-    });
-    
-    const payment = new Payment(client);
-
-    const body = ({
-            ip_adress:"123.444.555",
-            id:"01",
-            title: 'Mi producto',
-            unit_price: 64,
-            quantity: 1,
-            description: 'remera',
-            payment_method_id: 'Ars',
-            payer: {
-                email: 'ariass@gmail.com',
-                first_name:"comprador",
-                last_name:"comprador",
-                phone:{ area_code:"2625",number:"334455" },
-                adrdress:{zip_code:"5620",street_name:"vllc",street_number:"14600"}
-
-                },
-        purpose: 'wallet_purchase'
-    })
-        payment.create({ body }).then(console.log).catch(console.log);
-        console.log("BODY",body)
-        console.log("BODY",body)
-        console.log("PAYMENT",payment)
-    return res.status(200).json({msg:'hola'})*/
